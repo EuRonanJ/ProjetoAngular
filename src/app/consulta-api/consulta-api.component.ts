be308248b282418dfe8ec1e81054b496430030c6
Ronan <Ronan.tjq@gmail.com>
2025-02-10 21:35:57 -0300

    Alterações na consulta-api, html e css

--- src/app/consulta-api/consulta-api.component.ts
+++ src/app/consulta-api/consulta-api.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 type DogApiResponse = {
   url: string;
-  breeds: { name: string }[];
+  breeds: {
+     name: string, 
+     temperament: string, 
+     life_span: string }[];
 }
 
 @Component({
@@ -18,18 +21,26 @@ type DogApiResponse = {
 export class ConsultaApiComponent {
   dogImage: string | null = null;
   dogBreedName: string = 'Raça desconhecida';
+  dogTemperament: string = '';
+  dogLifeSpan: string = '';
 
   private apiUrl = 'https://api.thedogapi.com/v1/images/search?has_breeds=true&limit=1';
+  private apiKey = 'live_2ATUNr2MIzw5kvx5UCA22sNGn0vvF7htqwuxC9bQQyrvGLb0e1a3fF1siA7DItMi'; //Sem o código da API Nome, Descrição e Expectativa de vida serão sempre desconhidas 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getRandomDog(): void {
-    this.http.get<DogApiResponse[]>(this.apiUrl).subscribe(dogs => {
-      if (dogs.length > 0) {
-        const dogData = dogs[0];
-        this.dogImage = dogData.url;
-        this.dogBreedName = dogData.breeds?.[0]?.name || 'Raça desconhecida';
-      }
+    const headers = new HttpHeaders({
+      'x-api-key': this.apiKey
+    });
+
+    this.http.get<DogApiResponse[]>(this.apiUrl, { headers }).subscribe(dogs => {
+      const dogData = dogs[0];
+
+      this.dogImage = dogData.url;
+      this.dogBreedName = dogData.breeds?.[0]?.name || 'Raça desconhecida';
+      this.dogTemperament = dogData.breeds?.[0]?.temperament || 'Descrição não disponível';
+      this.dogLifeSpan = dogData.breeds?.[0]?.life_span || 'Estimativa de vida não disponível';
     });
   }
 }
